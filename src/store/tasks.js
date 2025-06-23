d6da8ffae7796db331db32987fd8ebce5bf88c19
thuanvo2405 <vothuan2405003@gmail.com>
2025-06-24 06:02:04 +0700

    create Slice

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,32 +1,25 @@
-import { createAction, createReducer,createSlice } from "@reduxjs/toolkit";
-
-
-// Action type
-const ADD_TASK = "ADD_TASK";
-const REMOVE_TASK = "REMOVE_TASK";
-const COMPLETED_TASK = "COMPLETED_TASK";
-
-// Action
-export const addTask = createAction("ADD_TASK");
-export const removeTask = createAction("REMOVE_TASK");
-export const completedTask = createAction("COMPLETED_TASK");
-
-// Reducer
+import { createSlice } from "@reduxjs/toolkit";
 
 let id = 0;
 
-export default createReducer([], {
-  [addTask.type]: (state, action) => {
-    state.push({ id: id++, task: action.payload.task, complete: false });
-  },
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState: [],
+  reducers: {
+    addTask: (state, action) => {
+      state.push({ id: id++, task: action.payload.task, complete: false });
+    },
+    removeTask: (state, action) => {
+      const index = state.findIndex((task) => task.id === action.payload.id);
+      state.splice(index, 1);
+    },
 
-  [removeTask.type]: (state, action) => {
-    const index = state.findIndex((task) => task.id === action.payload.id);
-    state.splice(index, 1);
-  },
-
-  [completedTask.type]: (state, action) => {
-    const index = state.findIndex((task) => task.id === action.payload.id);
-    state[index].complete = true;
+    completedTask: (state, action) => {
+      const index = state.findIndex((task) => task.id === action.payload.id);
+      state[index].complete = true;
+    },
   },
 });
+
+export const { addTask, removeTask, completedTask } = taskSlice.actions;
+export default taskSlice.reducer;
